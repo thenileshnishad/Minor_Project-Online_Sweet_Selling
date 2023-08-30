5c14fc89076f2c1f337c9203d315df2c2f5cbe16
thenileshnishad <thenileshnishad@gmail.com>
2023-08-31 01:04:28 +0530

    Structuring the SignUp page, with DB Connection

---
+++ customer/signup_code.php
@@ -0,0 +1,30 @@
+<?php
+require '../vendor/autoload.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $address = $_POST["address"];
+    $mobno = $_POST["mobno"];
+
+    $mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+    $database = $mongoClient->sweetselling;
+    $collection = $database->customers;
+
+    $insertResult = $collection->insertOne([
+        'name' => $name,
+        'email' => $email,
+        'password' => $password,
+        'address' => $address,
+        'mobno' => $mobno
+    ]
+    );
+
+    if ($insertResult->getInsertedCount() > 0) {
+        echo "User registered successfully!";
+    } else {
+        echo "User registration failed.";
+    }
+}
+?>
