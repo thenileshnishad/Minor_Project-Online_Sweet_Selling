62717d430b2d9c3520b1c166c63f00ab8b4b7f75
thenileshnishad <thenileshnishad@gmail.com>
2023-09-02 07:51:57 +0530

    Merging the SignUp Codes into One file

--- customer/signup.php
+++ customer/signup.php
@@ -4,7 +4,7 @@
 </head>
 <body>
     <h2>Customer Sign Up</h2>
-    <form action="signup_code.php" method="post">
+    <form action="" method="post">
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" required><br><br>
 
@@ -26,3 +26,34 @@
     </form>
 </body>
 </html>
+
+<?php
+require '../vendor/autoload.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $address = $_POST["address"];
+    $mobno = $_POST["mobno"];
+
+    $mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+    $database = $mongoClient->sweetselling;
+    $collection = $database->customers;
+
+    $insertResult = $collection->insertOne([
+        'name' => $name,
+        'email' => $email,
+        'password' => $password,
+        'address' => $address,
+        'mobno' => $mobno
+    ]
+    );
+
+    if ($insertResult->getInsertedCount() > 0) {
+        header('Location: signin.html');
+    } else {
+        echo "User registration failed.";
+    }
+}
+?>
