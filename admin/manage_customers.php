28cba4e7edac6b8710ea494dd4026a038c2fc748
thenileshnishad <thenileshnishad@gmail.com>
2023-09-10 08:06:55 +0530

    Restructuring the manage_customers page

--- admin/manage_customers.php
+++ admin/manage_customers.php
@@ -1,8 +1,44 @@
-<html>
+<?php
+
+if (isset($_SESSION["signedin"]) == true) {
+?>
+
+    <html>
+        
     <head>
         <link rel="stylesheet" href="">
     </head>
+
     <body>
-        <h2>This is customer manage page</h2>
+        <table class="user-table">
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Mobile</th>
+                <th>Delete</th>
+            </tr>
+
+            <?php
+            require '../vendor/autoload.php';
+
+            $mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+            $database = $mongoClient->sweetselling;
+            $collection = $database->customers;
+
+            $customerList = $collection->find(); ?>
+
+            <?php foreach ($customerList as $list) { ?>
+                <tr>
+                    <td><?php echo $list["name"]; ?></td>
+                    <td><?php echo $list["email"]; ?></td>
+                    <td><?php echo $list["address"]; ?></td>
+                    <td><?php echo $list["mobno"]; ?></td>
+                    <td><a href="delete_customer.php?delcustomer=<?php echo $list["_id"]; ?>" class="delete-link">Delete</a></td>
+                </tr>
+            <?php } ?>
+        </table>
     </body>
-</html>
+
+    </html>
+    <?php } ?>;
