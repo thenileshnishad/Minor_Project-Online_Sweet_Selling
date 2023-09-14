870af94ff9214a9fdbf6136e443e3828cc9595f3
thenileshnishad <thenileshnishad@gmail.com>
2023-09-14 21:27:49 +0530

    Initial navbar setup

--- customer/index.php
+++ customer/index.php
@@ -3,8 +3,24 @@ session_name("customer");
 session_start();
 
 if (isset($_SESSION["signedin"]) == true) {
-    echo "Welcome ". $_SESSION["custName"];
-    echo '<br><br><a href="logout.php">Logout</a>';
+?>
+    <html>
+
+    <head>
+        <title>Online Sweet Selling</title>
+    </head>
+
+    <body>
+        <h1>Welcome to Online Sweet Selling</h1>
+        <div class="nav">
+            <a href="index.php">Home</a>
+            <a href="logout.php">Logout</a>
+        </div>
+    </body>
+
+    </html>
+<?php
+    echo "Welcome " . $_SESSION["custName"];
 } else {
     header("Location: signin.php");
 }
