7bccd5ba5a13d5b4b0d6a26b0ccb0d0c4cb55911
thenileshnishad <thenileshnishad@gmail.com>
2023-09-24 20:30:27 +0530

    Refactor manage-contacts us page with db retrieval

--- admin/manage_contacts.php
+++ admin/manage_contacts.php
@@ -8,7 +8,36 @@ if (isset($_SESSION["signedin"]) == true) { ?>
 
     <body>
         <h2>Customer Messages</h2>
-        <h4>Here, you can see the messages sent by customers</h4>
+        <table class="user-table" border="1">
+            <tr>
+                <th>Customer name</th>
+                <th>Email</th>
+                <th>Regarding (Sweet name)</th>
+                <th>Subject</th>
+                <th>Message</th>
+                <th>Delete</th>
+            </tr>
+
+            <?php
+            require '../vendor/autoload.php';
+
+            $mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+            $database = $mongoClient->sweetselling;
+            $collection = $database->contact_forms;
+
+            $contacts = $collection->find(); ?>
+
+            <?php foreach ($contacts as $list) { ?>
+                <tr>
+                    <td><?php echo $list["name"]; ?></td>
+                    <td><?php echo $list["email"]; ?></td>
+                    <td><?php echo $list["regsweetname"]; ?></td>
+                    <td><?php echo $list["subject"]; ?></td>
+                    <td><?php echo $list["message"]; ?></td>
+                    <td>Delete</td>
+                </tr>
+            <?php } ?>
+        </table>
     </body>
 
     </html>
